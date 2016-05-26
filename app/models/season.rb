40a01cfe01eebf2e99580124015c916834019673
levanmanh <le.van.manh@framgia.com>
2016-05-26 11:10:14 +0700

    create-season

--- app/models/season.rb
+++ app/models/season.rb
@@ -3,4 +3,11 @@ class Season < ActiveRecord::Base
   has_many :matches
   has_many :posts
   has_many :team, through: :season_teams 
+
+  validates :name, presence: true, length: {maximum: 50}
+  validates :description, presence:  true, length: {maximum: 100}
+  validates :number_of_team, presence: true, numericality:
+    {only_integer: true, greater_than: 0}
+  validates :number_of_match, presence: true, numericality:
+    {only_integer: true, greater_than: 0}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root "static_pages#home"
   get "static_pages/help"
+
+  namespace :admin do
+    resources :seasons
+  end
 end
